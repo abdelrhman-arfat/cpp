95f3996ea9249941dfdda8cbb66027ee898f7b15
boody <abdoarfat2006@gmail.com>
2025-03-07 02:46:31 +0200

    age calculater with c++

---
+++ projects/age_calc/age_calc.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout << "What is the year which you born on it: ";
+  cin >> age;
+  int currentYear = 2025;
+  if (age > currentYear)
+  {
+    cout << "The age is not valid it should be less than " << currentYear << "." << endl;
+    return 0;
+  }
+
+  int years = currentYear - age;
+  cout << "You are " << years << " old" << endl;
+
+  int months = years * 12;
+  cout << "You are " << months << " months old" << endl;
+
+  // ignore using months * 30 because there are months with 28 or 29 or 30 or 31 days
+  int days = years * 365;
+  cout << "You are " << days << " days old" << endl;
+
+  int hours = days * 24;
+  cout << "You are " << hours << " hours old" << endl;
+
+  int minutes = hours * 60;
+  cout << "You are " << minutes << " minutes old" << endl;
+
+  int seconds = minutes * 60;
+  cout << "You are " << seconds << " seconds old" << endl;
+
+  return 0;
+}
